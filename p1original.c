25ea33e2d45d1fe50902d35af7a0cb65ef1652fb
charu13 <94678591+charu-12@users.noreply.github.com>
2022-04-20 11:40:20 +0000

    done

--- p1original.c
+++ p1original.c
@@ -5,20 +5,20 @@ void input(float*x1, float*x2, float*y1, float*y2)
   printf("Enter the value \n");
   scanf("%f%f%f%f",x1,x2,y1,y2);
 }
-void find_output(float x1, float x2,float y1, float y2, float*area)
+void find_output(float x1, float x2,float y1, float y2, float*distance)
 {
-  *area= sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+  *distance= sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 }
-void output(float x1, float x2,float y1, float y2, float area)
+void output(float x1, float x2,float y1, float y2, float distance)
 {
-  printf("Therefore the distance between points are %f",area);
+  printf("Therefore the distance between points are %f",distance);
   
 }
 int main()
 {
   float x1,x2,y1,y2,area;
   input(&x1,&x2,&y1,&y2);
-  find_output(x1,x2,y1,y2,&area);
+  find_output(x1,x2,y1,y2,&distance);
   output(x1,x2,y1,y2,area);
   return 0;
   }
---
+++ p7original.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+typedef struct point
+{
+ float x, y;
+} Point;
+typedef struct line
+{
+ Point p1, p2;
+ float distance;
+} Line;
+Point input_point()
+{
+ Point p;
+ scanf("%f%f", &p.x, &p.y);
+ return p;
+}
+Line input_line()
+{
+ Line l;
+ printf("Enter the first point:\n");
+ l.p1 = input_point();
+ printf("Enter the second point:\n");
+  l.p2 = input_point();
+ return l;
+}
+void find_length(Line* l)
+{
+  l->distance = sqrt((l->p1.x - l->p2.x) * (l->p1.x - l->p2.x) + 
+  (l->p1.y - l->p2.y) * (l->p1.y - l->p2.y));
+}
+void output(Line l)
+{
+ printf("The length of line with endpoints (%f,%f) and (%f,%f) is %f\n", l.p1.x, l.p1.y, l.p2.x,
+l.p2.y, l.distance);
+}
+int main()
+{
+ Line l = input_line();
+ find_length(&l);
+ output(l);
+ return 0;
+}
